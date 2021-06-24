78b9ed5760f9ddddb90994bc61527150706b28d6
Johnviti <84483840+Johnviti@users.noreply.github.com>
2021-06-24 14:21:49 -0300

    Game projetx

---
+++ main.py
@@ -0,0 +1,31 @@
+
+def ben_vindo(text):
+    tam= len(text) + 4
+    print("~" * tam)
+    print(f"  {text}")
+    print("~" * tam)
+
+ben_vindo("Bem vindo")
+print("~" * 36)
+x= int(input(f"Presione (1) pra fazer login \nPresione (2) pra criar conta"))
+print("~" * 36)
+
+if(x==1):
+    nick=input("Digite seu nick:")
+    senha=input("Sua senha:")
+else:
+    print("criar cadastro!")
+    cadastro=input("Digite seu nick:")
+    Senha=input("Digite uma senha:")
+
+
+
+def x(tex):
+    tam = len(tex) + 4
+    print("~" * tam)
+    print(f"Opa! {tex}, Vamos jogar!?")
+    print("~" * tam)
+
+
+
+
